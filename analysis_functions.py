f78cd739c044b80b42eddd154c230b627ea738e7
jowen005 <jowen005@odu.edu>
2024-11-27 18:14:32 -0500

    Incorporate into pltr function

--- analysis_functions.py
+++ analysis_functions.py
@@ -1,7 +1,14 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import make_classification
+from sklearn.model_selection import  train_test_split
 from sklearn.preprocessing import Binarizer
 import numpy as np
+
+def pltr(dataframe):
+    X = dataframe
+    y = dataframe['Label'].to_numpy()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, stratify=y)
+    decision_trees(X_train, y_train)
 
 def decision_trees(X, primary, secondary, y):
     # Only use the two passed columns to create the decision tree
@@ -9,4 +16,6 @@ def decision_trees(X, primary, secondary, y):
 
     dtree = DecisionTreeClassifier(max_depth=2)
     dtree.fit(dt_X,y)
+
+
 
