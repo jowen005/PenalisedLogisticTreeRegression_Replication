50ec893e08924886a8f1d52841ce949023475554
jowen005 <jowen005@odu.edu>
2024-12-05 02:01:43 -0500

    Everything is in but cross validation. Bugs still need to be fixed

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import importlib
+
+import pandas as pd
+
 import analysis_functions
+import numpy as np
 importlib.reload(analysis_functions)
 from analysis_functions import *
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.datasets import make_classification
 from sklearn.model_selection import  train_test_split
-from sklearn.preprocessing import Binarizer
-import numpy as np
-import matlab.engine
-import AdaptoLogit as al
+from sklearn.impute import SimpleImputer
+
 
 # Load the data
 dataframe = pd.read_csv("cs-test.csv")
+dataframe.rename(columns={'SeriousDlqin2yrs': 'Label'}, inplace=True)
 # Fill in missing values by imputing by mean
-
-# Standardize the data
+# Imputation by mean
+imp_mean = SimpleImputer(missing_values=pd.NA, strategy='mean')
+for col in dataframe.columns:
+    if dataframe[col].isnull().to_numpy().any():
+        imp_mean.fit(dataframe[col].to_numpy().reshape(-1, 1))
+        dataframe[col] = imp_mean.transform(dataframe[col].to_numpy().reshape(-1, 1))
 
 # Separate the labels from the rest of the data
 y = dataframe['Label'].to_numpy()
